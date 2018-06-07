8eecbb9879f68b1681a177e37d4ffd87878bd4b6
Derek Hirotsu <dhirotsu@ventanaconstruction.com>
2018-06-07 11:14:28 -0700

    changed behaviour of the CMD.clearAll function to clear screen instead of scrolling down to cover up lines

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const config = require('./.before-push.js')
 const package = require('./package.json')
 
 const CMD = {
-  clearAll: () => { process.stdout.write('\u001b[2J\u001b[0;0H') },
+  clearAll: () => { process.stdout.write('\u001b[H\u001b[J') },
   clear: line => readline.clearLine(process.stdout, 0, line),
-  write: msg => { process.stdout.write(`${msg}\n`); }
+  write: msg => { process.stdout.write(`${msg}\n`); },
+  read: () => { readline.createInterface({ input: process.stdin, output: process.stdout, }) },
 }
 
 const commit = {
@@ -20,7 +21,7 @@ const commit = {
 
 /* Main ========================================================================================= */
 const main = () => {
-  readline.createInterface({ input: process.stdin, output: process.stdout, })
+  CMD.read()
   
   const sceneManager = new SceneManager()
   sceneManager.push(new TypePrompt())
@@ -54,7 +55,14 @@ class SceneManager {
       this.index++
     } else {
       CMD.clearAll()
-      process.stdout.write('done');
+      // process.stdout.write('done');
+      CMD.write('done');
+      // CMD.write(`You chose: ${config.types[this.scenes[this.index].index].name}`)
+      /*
+      let test = Object.entries(commit.type)
+      test.map((x) => {
+        CMD.write(x)
+      })*/
     }
   }
 }
@@ -87,17 +95,20 @@ class TypePrompt extends Scene {
           this.index = this.index - 1
         }
 
-        CMD.clearAll(config.types.length)
+        // CMD.clearAll(config.types.length)
+        CMD.clearAll()
         this.draw()
       break
       case 'down':
         this.index = this.index + 1
         this.index = this.index % config.types.length
 
-        CMD.clearAll(config.types.length)
+        // CMD.clearAll(config.types.length)
+        CMD.clearAll()
         this.draw()
       break
       case 'return':
+        commit.type = config.types[this.index]
         this.next() // Assigned at SceneManager.push
       break
     }
@@ -108,10 +119,10 @@ class TypePrompt extends Scene {
     
     config.types.map((type, key) => {
       if (key === this.index) {
-        CMD.clear(key)
+        // CMD.clear(key)
         CMD.write(`> ${type.name}`)
       } else {
-        CMD.clear(key)
+        // CMD.clear(key)
         CMD.write(` ${type.name}`)
       }
     })
