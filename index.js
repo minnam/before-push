44b8724dcd9b2f0bf81d82c81815f6881eaa02cb
Min Nam <mnam@ventanaconstruction.com>
2018-06-08 13:59:55 -0700

    feat: modifies commit object when prompt manager finishes

--- index.js
+++ index.js
@@ -1,61 +1,52 @@
 const readline = require('readline')
+const ansiRegex = require('ansi-regex')
 const config = require('./.before-push.js')
 const package = require('./package.json')
 
 const CMD = {
+  interface: readline.createInterface({ input: process.stdin, output: process.stdout, historySize: 0}),
   clearAll: () => { process.stdout.write('\u001b[H\u001b[J') },
   clear: line => readline.clearLine(process.stdout, 0, line),
-  write: msg => { process.stdout.write(`${msg}\n`); },
-  read: () => { readline.createInterface({ input: process.stdin, output: process.stdout, }) },
+  write: msg => { process.stdout.write(`${msg}\n`); }
 }
 
-const commit = {
+let commit = {
   type : {},
-  version: [],
+  isRelease: {},
+  version: package.version.split('.'),
   message: {
-    prefix: '',
-    content: '',
-    postfix: ''
+    content: ''
   }
 }
 
 /* Main ========================================================================================= */
 const main = () => {  
-  const sceneManager = new SceneManager()
-  sceneManager.push(new TypePrompt())
-  
-  // if (config.message) {
-  //   if (config.message.prefix) {
-  //     sceneManager.push(new MessagePrompt('Write prefix for your commit message'))
-  //   }
-  // }
-  sceneManager.push(new MessagePrompt('Write your commit message 1'))
-  sceneManager.push(new MessagePrompt('Write your commit message 2'))
-  
-  sceneManager.draw()
+  const promptManager = new PromptManager()
+  promptManager.push(new SelectPrompt('What type of commit is this?', 'type', config.types))
+  promptManager.push(new SelectPrompt('Is this a part of release?', 'isRelease', [{name: 'Yes'}, {name: 'No'}]))
+  promptManager.push(new MessagePrompt('Write your commit message?', 'message.content'))  
 
+  promptManager.draw()
+  
 }
 
-rl = readline.createInterface({ input: process.stdin, output: process.stdout, historySize: 0})
-
-/* SceneManager ================================================================================= */
-class SceneManager {  
+/* PromptManager ================================================================================= */
+class PromptManager {  
   constructor () {
     this.index = 0
-    this.scenes = []
+    this.prompts = []
     this.data = ''
-    this.stdin = process.openStdin();
     CMD.clearAll()
 
     this.handleData =  (data) => {
-      if (this.index !== null && this.scenes[this.index].handleData) {                
-        this.scenes[this.index].handleData(data)        
+      if (this.index !== null && this.prompts[this.index].handleData) {                
+        this.prompts[this.index].handleData(data)        
       }
     }
 
     this.handleKeypress = (str, key) => {
-      if (this.index !== null && this.scenes[this.index].handleKeypress) {
-        this.scenes[this.index].handleKeypress(str, key)
+      if (this.index !== null && this.prompts[this.index].handleKeypress) {
+        this.prompts[this.index].handleKeypress(str, key)
       }
     }
 
@@ -63,59 +54,54 @@ class SceneManager {
 
   push (scene) {
     scene.next = this.next.bind(this)
-    this.scenes.push(scene)
+    this.prompts.push(scene)
   }
 
   draw () {  
-    if (this.scenes[this.index].draw) {
-      this.scenes[this.index].draw()
+    if (this.prompts[this.index].draw) {
+      this.prompts[this.index].draw()
     }      
     
+    CMD.interface.input.setEncoding("ascii")
     
-
-    process.stdin.resume()
-    process.stdin.setEncoding("ascii")
-    
-    this.stdin.addListener('data', this.handleData)
-
-    this.stdin.addListener('end', () => {
-      if (this.index !== null && this.scenes[this.index].handleEnd) {                
-        this.scenes[this.index].handleEnd(data)        
-      }
-    })
-
-    this.stdin.addListener('keypress', this.handleKeypress)   
+    CMD.interface.input.addListener('data', this.handleData)
+    CMD.interface.input.addListener('keypress', this.handleKeypress)   
    
   }
 
   reset () {
     this.data = ''
-    this.stdin.removeListener('data', this.handleData)
-    this.stdin.removeListener('keypress', this.handleKeypress)
+    CMD.interface.input.removeListener('data', this.handleData)
+    CMD.interface.input.removeListener('keypress', this.handleKeypress)
 
     this.handleData =  (data) => {
-      if (this.index !== null && this.scenes[this.index].handleData) {                
-        this.scenes[this.index].handleData(data)        
+      if (this.index !== null && this.prompts[this.index].handleData) {
+        this.prompts[this.index].handleData(data)
       }
     }
 
     this.handleKeypress = (str, key) => {
-      if (this.index !== null && this.scenes[this.index].handleKeypress) {
-        this.scenes[this.index].handleKeypress(str, key)
+      if (this.index !== null && this.prompts[this.index].handleKeypress) {
+        this.prompts[this.index].handleKeypress(str, key)
       }
     }
   }
 
   next () {
     this.reset()
-    if (this.index + 1 < this.scenes.length) {      
+    if (this.index + 1 < this.prompts.length) {
+      process.stdout.clearLine();
       this.index++
       this.reset()
-      this.draw()      
+      this.draw()
     } else {                        
-      console.log(this.index)
       process.stdin.unref()
-      this.index = null      
+      this.index = null
+      
+      commit.version[commit.type.level]++
+
+      console.log(commit)
+
     }
   }
 }
@@ -123,23 +109,53 @@ class SceneManager {
 /**
  * Use the Scene class as a template to write prompts
  */
-class Scene {
-  constructor () {
+class Prompt {
+  constructor (question, key) {
     this.index = 0
+    this.question = question
+    this.key = key
+    this.data = ''
+  }
+
+  process (data) {
+    const parsedKey = this.key.split('.')
+    let i = parsedKey.length - 1
+    let object = null
+
+    while(i > -1) {      
+      if (!object) {
+        object = {}
+        object[parsedKey[i]] = data
+      } else {
+        const temp = {...object}
+        object = {}
+        object[parsedKey[i]] = {
+          ...temp
+        }        
+      }
+      i--
+    }
+    
+    commit = {
+      ...commit,
+      ...object
+    }
   }
 }
 
-/* TypePrompt ====================================================================================*/
-class TypePrompt extends Scene {
-  constructor () {
-    super(null)
+/* SelectPrompt ====================================================================================*/
+class SelectPrompt extends Prompt {
+  constructor (question, key, items) {
+    super(question, key)
+    this.isRaw = true
+    this.items = items
   }
 
   handleKeypress (str, key) {    
     switch (key.name) {
       case 'up':
         if (this.index - 1 === -1) {
-          this.index = config.types.length - 1
+          this.index = this.items.length - 1
         } else {
           this.index = this.index - 1
         }
@@ -149,14 +165,14 @@ class TypePrompt extends Scene {
       break
       case 'down':
         this.index = this.index + 1
-        this.index = this.index % config.types.length
+        this.index = this.index % this.items.length
 
         CMD.clearAll()
         this.draw()
       break
       case 'return':
-        commit.type = config.types[this.index]
-        this.next() // Assigned at SceneManager.push
+        this.process(this.items[this.index])
+        this.next() // Assigned at PromptManager.push
       break
       default:
         // CMD.clearAll()
@@ -165,47 +181,40 @@ class TypePrompt extends Scene {
   }
 
   draw () {
-    CMD.write(`What kind of commit is it?`)
+    CMD.write(this.question)
     
-    config.types.map((type, key) => {
+    this.items.map((type, key) => {
       if (key === this.index) {
-        CMD.write(`> ${type.name}`)
+        CMD.write(`● ${type.name}`)
       } else {
-        CMD.write(` ${type.name}`)
+        CMD.write(`○ ${type.name}`)
       }
     })
   }
 }
 
-class MessagePrompt extends Scene {
-  constructor (message) {
-    super(null)
-    this.question = `${message}`
+class MessagePrompt extends Prompt {
+  constructor (question, key) {    
+    super(question, key)
+    this.isRaw = false
   }
 
   handleData (chunk) {        
-    this.data += chunk
+    this.data += chunk.replace(ansiRegex(), '').replace(/\r/g, '')
   }
 
   handleKeypress (str, key) {
     switch (key.name) {
-      case 'return':
-        commit.message = this.data
-        this.next() // Assigned at SceneManager.push
+      case 'return':        
+        this.process(this.data)
+        this.next() // Assigned at PromptManager.push
       break
-      default:
-        
     }
-  }
-
-  handleEnd () {
-    this.next()
   }
 
   draw () {
     CMD.write(this.question)
-    rl.prompt()
-    //CMD.write(this.data)
+    CMD.interface.prompt()
   }
 }
 
