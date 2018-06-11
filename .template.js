c6edff482a1c112d689856e06f88c14e16db7a3a
Derek Hirotsu <dhirotsu@ventanaconstruction.com>
2018-06-11 11:03:51 -0700

    Config changes
    - Added a template changlog for temporary use
    - updated packages to use eslint formatting

--- .template.js
+++ .template.js
@@ -1,13 +1,50 @@
 const configTemplate = {
+    // Each object in the commiType array will be represented as a choice when the script inintially prompts
+    // for a type of commit.
     commitType: [
         {
-            name: 'name',
-            commitMsg: true,
-            ...(commitMsg && { prefix: true }),
-            ...(commitMsg && { postfix: true }),
-            updateLog: true,
-            ...(updateLog && { date: true }),
-            ...(updateLog && { verNo: true }),
+            typeName: 'name', // string           
+            // The name of the commit type that is displayed in the commit type propmt.
+
+            requrieMessage: true, // boolean   
+            // Specifies whether a commit message is required or not.
+
+            requrieMessagePrefix: true, // boolean  
+            // Determines if the commit message should have prefixed text.
+
+            requrieMessagePostfix: true, //boolean
+            // Determines if the commit message should have appended text.
+
+            updateChangelog: true, // boolean
+            // Determines if the changelog should be updated when running a commit of this type.
+
+            omitPrefix: true, // boolean
+            // Specifies if the prefix should be omitted from the changelog entry.
+            
+            omitPostfix: true, // boolean
+            // Specifies if the postfix should be omitted from the changelog entry.
+            
+            addDate: true, // boolean
+            // Determines if the changelog entry should be formatted with a date.
+
+            addVersionNumbering: true, // boolean           
+            // Determines if the changelog entry should be formatted with a version number.
+
+            versionIndex: 0, // integer
+            // Specifies which digit (index) of the changelog's version numbering to increment.
         }
-    ]
+    ],
+    versionFormat: {
+        useCustomVersion: false, // boolean
+        // Specifies if the version numbering displayed in the changelog should follow default numbering
+        // or the format specified in the 'custom' property.
+        
+        custom: '#.#.#' // string
+        // Defines a custom template for the changlog numbering.
+        // Digits are placed in the string at the locations designated by the '#' character.
+        // Examples:
+        // A version number like: 1.12.5 would use a format like: #.#.#
+        // A version number like: 0-1-6.1 would use a format like: #-#-#.# 
+    },
+    dateFormat: 'yyyy-mm-dd',
 }
--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const readline = require('readline')
 const ansiRegex = require('ansi-regex')
 const config = require('./.before-push.js')
-const package = require('./package.json')
+const _package = require('./package.json')
 
 const CMD = {
-  interface: readline.createInterface({ input: process.stdin, output: process.stdout, historySize: 0}),
+  interface: readline.createInterface({ input: process.stdin, output: process.stdout, historySize: 0 }),
   clearAll: () => { process.stdout.write('\u001b[H\u001b[J') },
   clear: line => readline.clearLine(process.stdout, 0, line),
-  write: msg => { process.stdout.write(`${msg}\n`); }
+  write: msg => { process.stdout.write(`${msg}\n`) }
 }
 
 let commit = {
   type : {},
   isRelease: {},
-  version: package.version.split('.'),
+  version: _package.version.split('.'),
   message: {
     content: ''
   }
 }
 
 /* Main ========================================================================================= */
-const main = () => {  
+const main = () => {
   const promptManager = new PromptManager()
   promptManager.push(new SelectPrompt('What type of commit is this?', 'type', config.types))
   promptManager.push(new SelectPrompt('Is this a part of release?', 'isRelease', [{name: 'Yes'}, {name: 'No'}]))
-  promptManager.push(new MessagePrompt('Write your commit message?', 'message.content'))  
+  promptManager.push(new MessagePrompt('Write your commit message?', 'message.content'))
 
   promptManager.draw()
-  
+
 }
 
 /* PromptManager ================================================================================= */
-class PromptManager {  
+class PromptManager {
   constructor () {
     this.index = 0
     this.prompts = []
@@ -39,8 +39,8 @@ class PromptManager {
     CMD.clearAll()
 
     this.handleData =  (data) => {
-      if (this.index !== null && this.prompts[this.index].handleData) {                
-        this.prompts[this.index].handleData(data)        
+      if (this.index !== null && this.prompts[this.index].handleData) {
+        this.prompts[this.index].handleData(data)
       }
     }
 
@@ -50,23 +50,23 @@ class PromptManager {
       }
     }
 
-  }  
+  }
 
   push (scene) {
     scene.next = this.next.bind(this)
     this.prompts.push(scene)
   }
 
-  draw () {  
+  draw () {
     if (this.prompts[this.index].draw) {
       this.prompts[this.index].draw()
-    }      
-    
-    CMD.interface.input.setEncoding("ascii")
-    
+    }
+
+    CMD.interface.input.setEncoding('ascii')
+
     CMD.interface.input.addListener('data', this.handleData)
-    CMD.interface.input.addListener('keypress', this.handleKeypress)   
-   
+    CMD.interface.input.addListener('keypress', this.handleKeypress)
+
   }
 
   reset () {
@@ -90,14 +90,14 @@ class PromptManager {
   next () {
     this.reset()
     if (this.index + 1 < this.prompts.length) {
-      process.stdout.clearLine();
+      process.stdout.clearLine()
       this.index++
       this.reset()
       this.draw()
-    } else {                        
+    } else {
       process.stdin.unref()
       this.index = null
-      
+
       commit.version[commit.type.level]++
 
       console.log(commit)
@@ -122,7 +122,7 @@ class Prompt {
     let i = parsedKey.length - 1
     let object = null
 
-    while(i > -1) {      
+    while(i > -1) {
       if (!object) {
         object = {}
         object[parsedKey[i]] = data
@@ -131,11 +131,11 @@ class Prompt {
         object = {}
         object[parsedKey[i]] = {
           ...temp
-        }        
+        }
       }
       i--
     }
-    
+
     commit = {
       ...commit,
       ...object
@@ -143,7 +143,7 @@ class Prompt {
   }
 }
 
-/* SelectPrompt ====================================================================================*/
+/* SelectPrompt ================================================================================= */
 class SelectPrompt extends Prompt {
   constructor (question, key, items) {
     super(question, key)
@@ -151,30 +151,30 @@ class SelectPrompt extends Prompt {
     this.items = items
   }
 
-  handleKeypress (str, key) {    
+  handleKeypress (str, key) {
     switch (key.name) {
-      case 'up':
-        if (this.index - 1 === -1) {
-          this.index = this.items.length - 1
-        } else {
-          this.index = this.index - 1
-        }
+    case 'up':
+      if (this.index - 1 === -1) {
+        this.index = this.items.length - 1
+      } else {
+        this.index = this.index - 1
+      }
 
-        CMD.clearAll()
-        this.draw()
+      CMD.clearAll()
+      this.draw()
       break
-      case 'down':
-        this.index = this.index + 1
-        this.index = this.index % this.items.length
+    case 'down':
+      this.index = this.index + 1
+      this.index = this.index % this.items.length
 
-        CMD.clearAll()
-        this.draw()
+      CMD.clearAll()
+      this.draw()
       break
-      case 'return':
-        this.process(this.items[this.index])
-        this.next() // Assigned at PromptManager.push
+    case 'return':
+      this.process(this.items[this.index])
+      this.next() // Assigned at PromptManager.push
       break
-      default:
+    default:
         // CMD.clearAll()
         // this.draw()
     }
@@ -182,7 +182,7 @@ class SelectPrompt extends Prompt {
 
   draw () {
     CMD.write(this.question)
-    
+
     this.items.map((type, key) => {
       if (key === this.index) {
         CMD.write(`● ${type.name}`)
@@ -194,20 +194,20 @@ class SelectPrompt extends Prompt {
 }
 
 class MessagePrompt extends Prompt {
-  constructor (question, key) {    
+  constructor (question, key) {
     super(question, key)
     this.isRaw = false
   }
 
-  handleData (chunk) {        
+  handleData (chunk) {
     this.data += chunk.replace(ansiRegex(), '').replace(/\r/g, '')
   }
 
   handleKeypress (str, key) {
     switch (key.name) {
-      case 'return':        
-        this.process(this.data)
-        this.next() // Assigned at PromptManager.push
+    case 'return':
+      this.process(this.data)
+      this.next() // Assigned at PromptManager.push
       break
     }
   }
